76eecff28ed65b9f1ea20ad1e81e139efcd85371
rajkumar <rajkumar.arisetty@mindfiresolutions.com>
2017-11-04 20:21:42 +0530

    Removed duplicate css files

--- app/main.jsx
+++ app/main.jsx
@@ -5,9 +5,6 @@ import configureStore from './stores/configureStore.jsx';
 import { Provider } from 'react-redux';
 import GroceryItemList from './components/GroceryItemList.jsx';
 import {loadGroceryItems} from './actions/groceryItemsActions.jsx';
-import './styles.css';
-import './../bower_components/skeleton/css/normalize.css';
-import './../bower_components/skeleton/css/skeleton.css';
 
 const store = configureStore();
 store.dispatch(loadGroceryItems());
