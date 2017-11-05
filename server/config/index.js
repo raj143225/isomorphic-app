3a4133b03e704bfd54c53a61d6af607d8335ac5d
rajkumar <rajkumar.arisetty@mindfiresolutions.com>
2017-11-05 14:24:11 +0530

    Removed hardcoded database confuguration

--- server/config/index.js
+++ server/config/index.js
@@ -5,7 +5,7 @@ module.exports= {
     getDbConnection :function() {
         return 'mongodb://' + configVariables.username +
             ':' + configVariables.password +
-            '@ds245885.mlab.com:45885/grocery';
+            configVariables.database;
     }
 };
 
