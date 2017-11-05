26e007035e077adca61e82b9ca699645a85383aa
rajkumar <rajkumar.arisetty@mindfiresolutions.com>
2017-11-05 20:51:42 +0530

    Removed not used lines

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import {RouterContext, match } from 'react-router';
-const hist = require('history');
 const router = express.Router();
 const app = express();
 const groceryItems = require('./../models/groceryItemModel');
@@ -18,11 +17,9 @@ router.get('/', function(req, res, next) {
     //res.render('./../../app/index.ejs', {});
     const store = createStore(counterApp);
 
-    //
-    const location = hist.createLocation(req.path);
     match({
         routes: appRoutes,
-        location: location
+        location: req.url
     }, (err, redirectLocation, renderProps) => {
         groceryItems.find(function(error, doc) {
             const generated = ReactDOMServer.renderToString(
